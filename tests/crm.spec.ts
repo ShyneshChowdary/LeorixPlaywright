ec446d64a6ab9ab6666d34610e029a833fe3b1f4
Shynesh Raparla <raparlashynesh64@gmail.com>
2026-04-03 20:20:20 +0530

    update crm.spec.ts

--- tests/crm.spec.ts
+++ tests/crm.spec.ts
@@ -21,11 +21,17 @@ test('Development test', async ({ page }) => {
   await page.goBack();
   await expect(page).toHaveURL('https://app-dev.foundershub.ai/modules?type=crm');
 
-  //await expect(page.getByText('Data Quality Heat Map').first()).toHaveCount(1);
-
   await expect(page.getByText('List').first()).toHaveCount(1);
   await page.getByText('List').first().click();
   await expect(page).toHaveURL('https://app-dev.foundershub.ai/modules?type=crm');
 
+  await page.getByText('Grid').first().click();
+  await expect(page).toHaveURL('https://app-dev.foundershub.ai/modules?type=crm');
+
+  await page.getByText('Prospects').first().click();
+  await expect(page).toHaveURL('https://app-dev.foundershub.ai/metrics/7fbb96a7-0b10-46ee-bde4-3e2e50888e70/data');
+
+  await page.getByText('Visualization').first().click();
+  await expect(page).toHaveURL('https://app-dev.foundershub.ai/metrics/7fbb96a7-0b10-46ee-bde4-3e2e50888e70/visualizations');
 
 });
